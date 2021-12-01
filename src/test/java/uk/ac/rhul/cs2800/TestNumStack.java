182bbbdf6727444b9a757f4950f21fd57326927e
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-01 23:46:50 +0000

    Created Junit test file for numstack class. Created an instantiation test

---
+++ src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
@@ -0,0 +1,17 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author qasim
+ *
+ */
+class TestNumStack {
+
+  @Test
+  void testNumStackCreate() {
+    NumStack stk = new NumStack();
+  }
+
+}
