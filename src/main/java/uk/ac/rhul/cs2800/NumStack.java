abde38e53701326d842212003bfd6038c6bcee1a
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-15 13:37:31 +0000

    inherited Numstack from Stack, passed tests.

--- src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -4,8 +4,8 @@ package uk.ac.rhul.cs2800;
  * @author qasim
  *
  */
-public class NumStack {
-  private Stack numStack;
+public class NumStack extends Stack {
+  private Stack numstack;
   
-
+  
 }
