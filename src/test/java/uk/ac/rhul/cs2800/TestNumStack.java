7c86679bc2002bfcc4688d671ea2707079e2fe33
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-16 03:01:12 +0000

    New NumStack tests created + added test comments

--- src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
@@ -3,22 +3,33 @@ package uk.ac.rhul.cs2800;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author qasim
- *
- */
 class TestNumStack {
 
   @Test
-  void testNumStackCreate() {
+  void testNumStackCreate() { // test 19- passed by creating NumsStack class.
     NumStack stk = new NumStack();
   }
 
   @Test
-  void testNumStackPush() {
+  void testNumStackPush() { // test 20- passed by inheriting from stack class.
     NumStack stk = new NumStack();
     Entry ent = new Entry(2);
     stk.push(ent);
   }
+  @Test
+  void testNumStackEval() { // test 21-
+    NumStack stk = new NumStack();
+    Entry ent = new Entry(2);
+    Entry ent2 = new Entry(2);
+    Symbol plus = Symbol.PLUS;
+    Entry ent3 = new Entry(plus);
+    stk.push(ent);
+    stk.push(ent2);
+    stk.push(ent3);
+    stk.evaluate();
+
+  }
+
+
 
 }
