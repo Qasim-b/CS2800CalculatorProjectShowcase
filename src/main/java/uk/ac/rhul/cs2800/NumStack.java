e4c8226a925ba207207d69135d07f33fa2453caf
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-16 22:13:01 +0000

    Rewrote Numstack class with new push,pop and isEmpty functions to pass tests.

--- src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -1,11 +1,45 @@
 package uk.ac.rhul.cs2800;
 
 /**
+ * A class used to evaluate reverse polish expressions.
+ *
  * @author qasim
  *
  */
-public class NumStack extends Stack {
-  private Stack numstack;
-  
-  
+public class NumStack {
+  private Stack numstack = new Stack();
+
+  /**
+   * Pushes a number onto the stack.
+   *
+   * @param i number to be pushed onto the stack
+   */
+  public void push(float i) {
+    Entry numentry = new Entry(i);
+    this.numstack.push(numentry);
+  }
+
+  /**
+   * Returns the top of the stack as a float value instead of Entry type.
+   *
+   * @return float value of entry in the numstack
+   */
+  public float pop() {
+    return this.numstack.pop().getValue();
+  }
+
+  /**
+   * Checks if numstack is empty.
+   *
+   * @return true if empty, false if not empty
+   */
+  public boolean isEmpty() {
+    if (this.numstack.size() == 0) {
+      return true;
+    }
+    return false;
+  }
+
+
+
 }
