e9b77df6c0663372ef56d89ece8c026e0eed69bb
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-16 22:44:12 +0000

    RevPolishCalc test file. first test, testing creation of RevPolishCalc.

---
+++ CS2800/branches/FeatureRPcalc/src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestRevPolishCalc {
+
+  @Test
+  void testRevPolishCalcCreate() {
+    RevPolishCalc rpcalc = new RevPolishCalc();
+  }
+
+}
