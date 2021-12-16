b59022e962e157939239df7d2a80ade3d2bad26f
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-16 23:19:12 +0000

    developing evaluate function, implemented numstack filling.

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -7,12 +7,19 @@ package uk.ac.rhul.cs2800;
  *
  */
 public class RevPolishCalc implements Calculator {
+  private NumStack values = new NumStack();
 
   /**
-   *Evaluates a given argument in reverse polish format.
+   * Evaluates a given argument in reverse polish format.
    */
   public float evaluate(String what) throws IllegalArgumentException {
-
+    String[] splitInput = what.split(" ");
+    String symbols = "*/+-";
+    for (String t : splitInput) {
+      if (!symbols.contains(t)) {
+        values.push(Float.parseFloat(t));
+      }
+    }
     return 0;
 
   }
