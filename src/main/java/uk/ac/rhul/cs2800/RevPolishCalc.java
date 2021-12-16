9ce9af751e86491c4762c50b00672432a960ab41
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-16 22:50:32 +0000

    initial setup of RevPolishCalc class, implementing Calculator interface.

---
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -0,0 +1,19 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * Evaluates reverse polish expressions.
+ *
+ * @author qasim
+ *
+ */
+public class RevPolishCalc implements Calculator {
+
+  /**
+   *Evaluates a given argument in reverse polish format.
+   */
+  public float evaluate(String what) throws IllegalArgumentException {
+
+    return 0;
+
+  }
+}
