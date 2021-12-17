ba495c7b566216b70abeb727912e491681ef309d
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-17 01:11:22 +0000

    Creating CalcModel file, setting private attributes.

---
+++ src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -0,0 +1,15 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * Model for combining both reverse polish and infix calculators.
+ *
+ * @author qasim
+ *
+ */
+public class CalcModel {
+  private RevPolishCalc rpCalc;
+  private float result;
+  private float[] memory; // store results in memory array.
+
+
+}
