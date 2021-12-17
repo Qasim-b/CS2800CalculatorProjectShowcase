35b440e02311c64d45f32d5c4ff9f8afab6d633d
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-17 01:15:19 +0000

    Changed CalcModel to TestCalcModel inside tests folder

---
+++ src/test/java/uk/ac/rhul/cs2800/TestCalcModel.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestCalcModel {
+
+  @Test
+  void testModelCreation() {
+    CalcModel calcmod = new CalcModel();
+  }
+
+}
