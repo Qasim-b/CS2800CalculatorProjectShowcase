814f25c00961144b79d19a1859dcb3fab1e7217b
zhac215 <zhac215@4793646a-61f7-4eaa-b358-82cdbdb52888>
2021-12-17 01:40:13 +0000

    added evaluate function to calcmodel, removed boolean check as not needed at the moment.

--- src/main/java/uk/ac/rhul/cs2800/CalcModel.java
+++ src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -1,5 +1,8 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Model for combining both reverse polish and infix calculators.
  *
@@ -7,9 +10,20 @@ package uk.ac.rhul.cs2800;
  *
  */
 public class CalcModel {
-  private RevPolishCalc rpCalc;
+  private RevPolishCalc rpCalc = new RevPolishCalc();
   private float result;
-  private float[] memory; // store results in memory array.
+  private float[] memory; // store results in memory arraylist.
+  private int counter = 0;
 
+  /**
+   * Evaluates the users input and applies it to the calculator they want to use.
+   *
+   * @param expression string representing the user input
+   * @return float calculated value from the expression
+   */
+  public float evaluate(String expression) {
+    this.result = this.rpCalc.evaluate(expression);
+    return this.result;
 
+  }
 }
